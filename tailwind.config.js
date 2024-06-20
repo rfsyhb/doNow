c4be631a25582444c4343a43c56dd506ede08f15
rfsyhb <rafisyihab3@gmail.com>
2024-06-20 22:11:19 +0700

    add color variables

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,12 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   theme: {
     extend: {
-      colors: {},
+      colors: {
+        bgColor: '#E6E5E3',
+        text: '#141314',
+        cardMain: '#F4F5F2',
+        cardAlt: '#EBEBEB',
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
       },
