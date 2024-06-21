8cb57af627e571a85d64f6693c1d37adbd594fbe
rfsyhb <rafisyihab3@gmail.com>
2024-06-21 21:37:28 +0700

    add action get data from localStorage

--- src/features/history/historySlice.js
+++ src/features/history/historySlice.js
@@ -1,14 +1,30 @@
 import { createSlice } from '@reduxjs/toolkit';
+import { getTodayDateKey } from '../../utils/helper';
+
+const loadHistory = () => {
+  const todayKey = getTodayDateKey();
+  const storedHistory = localStorage.getItem(todayKey);
+  return storedHistory ? JSON.parse(storedHistory) : [];
+};
+
+const saveHistory = (history) => {
+  const todayKey = getTodayDateKey();
+  localStorage.setItem(todayKey, JSON.stringify(history));
+};
 
 const historySlice = createSlice({
   name: 'history',
-  initialState: [],
+  initialState: loadHistory(),
   reducers: {
     addHistory: (state, action) => {
       state.push(action.payload);
+      saveHistory(state);
+    },
+    loadHistoryFromLocalStorage: () => {
+      return loadHistory();
     },
   },
 });
 
-export const { addHistory } = historySlice.actions;
+export const { addHistory, loadHistoryFromLocalStorage } = historySlice.actions;
 export default historySlice.reducer;
