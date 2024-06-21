002bbb55e21b59a0b35deec76a71339516680d9f
rfsyhb <rafisyihab3@gmail.com>
2024-06-21 21:24:13 +0700

    styling for history

--- src/components/History.jsx
+++ src/components/History.jsx
@@ -4,14 +4,29 @@ import { useSelector } from 'react-redux';
 
 function History() {
   const history = useSelector((state) => state.history);
+  const date = new Date();
+  const day = date.getDate();
+  const month = date.toLocaleString('default', { month: 'long' });
+  const year = date.getFullYear();
+  const todayDate = `${day} ${month} ${year}`;
 
   return (
-    <div>
-      <h2>History</h2>
-      <ul>
+    <div className="flex flex-col bg-cardMain p-4 rounded-xl border border-black border-opacity-20">
+      <div className="flex flex-row items-center justify-between px-10 pb-3">
+        <h2 className="text-xl font-semibold">History</h2>
+        <p className="text-sm">{todayDate}</p>
+      </div>
+      <ul className="flex flex-col gap-1">
         {history.map((item, index) => (
-          <li key={index}>
-            {item.title} - {item.time} min
+          <li
+            key={index}
+            className="flex flex-row justify-between gap-10 bg-cardAlt p-1 px-3 rounded-xl border border-black border-opacity-10"
+          >
+            <p>{item.title}</p>
+            <p>
+              {item.time}
+              <span className="text-sm">m</span>
+            </p>
           </li>
         ))}
       </ul>
